1a1fa0f67a73afee514324c0112c018aecf9c75f
FazleRifat121 <fazlerifatofficial@gmail.com>
2025-03-16 22:29:20 +0600

    2nd part of home text body done

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -5,14 +5,18 @@ const Home = () => {
     <div className="max-w-screen mx-auto p-4 h-[100dvh]">
       <div className="flex justify-center items-center flex-col gap-5">
         <img src={img1} alt="logo" />
-        <div className="text-body flex flex-col items-center justify-between">
+        <div className="text-body flex flex-col items-center justify-center">
           <div className="first-part text-center space-y-5">
             <h1 className="text-7xl font-extrabold">Evoca Labs</h1>
             <p className="text-lg font-semibold tracking-widest">
               Ai-Powered Memory Assistance
             </p>
           </div>
-          <div className="second-part mt-20">
+          <div className="second-part mt-10 space-y-3 flex flex-col items-center justify-center">
+            <p className="text-lg font-semibold tracking-widest text-center">
+              Be the first to know when we launch! <br />
+              Sign up for updates.
+            </p>
             <Search></Search>
           </div>
         </div>
