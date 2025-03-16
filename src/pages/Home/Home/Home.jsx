334ada34259fccc7905b75fca4b80911ee434a0b
FazleRifat121 <fazlerifatofficial@gmail.com>
2025-03-16 23:44:53 +0600

    home page done

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,14 +1,18 @@
-import { FaFacebook } from "react-icons/fa";
 import img1 from "../../../assets/Home/logo.png";
+import fb from "../../../assets/Home/fb.svg";
+import twitter from "../../../assets/Home/twitter.svg";
+import email from "../../../assets/Home/email.svg";
+import title from "../../../assets/Home/title.svg";
 import Search from "./Search/Search";
+import { Link } from "react-router";
 const Home = () => {
   return (
-    <div className="max-w-screen mx-auto p-4 text-white">
+    <div className="max-w-screen mx-auto p-4 text-white font-diavlo">
       <div className="flex justify-center items-center flex-col gap-5  h-[100dvh]">
         <img src={img1} alt="logo" />
         <div className="text-body flex flex-col items-center justify-center">
           <div className="first-part text-center space-y-5">
-            <h1 className="md:text-7xl text-5xl  font-extrabold">Evoca Labs</h1>
+            <img src={title} alt="Evoca Labs" />
             <p className="text-lg font-semibold tracking-widest">
               Ai-Powered Memory Assistance
             </p>
@@ -25,9 +29,22 @@ const Home = () => {
             <h3 className="text-xl font-medium tracking-widest">
               Stay in touch :
             </h3>
-            <ul className="flex items-center gap-2">
-              <li className="text-2xl flex items-center ">
-                <FaFacebook className="" />
+            <ul className="flex items-center gap-4 ">
+              <li className="text-2xl flex items-center  ">
+                <Link to={"remove this text enter your link here"}>
+                  <img src={fb} alt="" />
+                </Link>
+              </li>
+
+              <li className="text-2xl flex items-center  ">
+                <Link to={"remove this text enter your link here"}>
+                  <img src={twitter} alt="" />
+                </Link>
+              </li>
+              <li className="text-2xl flex items-center  ">
+                <Link to={"remove this text enter your link here"}>
+                  <img src={email} alt="" />
+                </Link>
               </li>
             </ul>
           </div>
