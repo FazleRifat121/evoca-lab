e50a4dcc99063cf3847487d40f23a3043cabe80d
FazleRifat121 <fazlerifatofficial@gmail.com>
2025-03-18 23:27:47 +0600

    footer done

---
+++ src/shared/Footer/Footer.jsx
@@ -0,0 +1,37 @@
+import logo from "../../assets/logo.png";
+import { FaInstagram } from "react-icons/fa6";
+import { FaFacebookSquare } from "react-icons/fa";
+import { BsTwitterX } from "react-icons/bs";
+const Footer = () => {
+  return (
+    <footer className="footer sm:footer-horizontal text-neutral-content items-center p-4">
+      <aside className="grid-flow-col items-center">
+        <a href="/">
+          {" "}
+          <img src={logo} alt="logo" />
+        </a>
+      </aside>
+      <div>
+        <p>
+          Terms & conditions Privacy policy Cookies policy Copyrights
+          Notification
+        </p>
+        <span className="text-gray-500 md:mx-auto">All rights reserved.</span>
+      </div>
+
+      <nav className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
+        <a href="link">
+          <FaInstagram className="text-3xl text-[#C8DA27]" />
+        </a>
+        <a href="link">
+          <FaFacebookSquare className="text-3xl text-[#C8DA27]" />
+        </a>
+        <a href="link">
+          <BsTwitterX className="text-3xl text-[#C8DA27]" />
+        </a>
+      </nav>
+    </footer>
+  );
+};
+
+export default Footer;
