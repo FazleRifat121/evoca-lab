6fbbd385af6ea7b7f9481f359a7cbe72f12ef4e2
FazleRifat121 <fazlerifatofficial@gmail.com>
2025-03-18 21:25:15 +0600

    banner done

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -10,6 +10,12 @@ export const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayout />}>
       <Route path="/" element={<Home />}></Route>
+      <Route path="/about" element={<Home />}></Route>
+      <Route path="/team" element={<Home />}></Route>
+      <Route path="/sales" element={<Home />}></Route>
+      <Route path="/story" element={<Home />}></Route>
+      <Route path="/videos" element={<Home />}></Route>
+      <Route path="/contact" element={<Home />}></Route>
     </Route>
   )
 );
