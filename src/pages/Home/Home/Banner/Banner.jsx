7f9d3ca7348fa89519780caf3bb87814e5e76d8d
FazleRifat121 <fazlerifatofficial@gmail.com>
2025-03-18 20:16:24 +0600

    banner added

--- src/pages/Home/Home/Banner/Banner.jsx
+++ src/pages/Home/Home/Banner/Banner.jsx
@@ -1,5 +1,17 @@
+import robot from "../../../../assets/Home/Banner/robot.png";
+import AI from "../../../../assets/Home/Banner/AI-powered memory assistance.png";
+
 const Banner = () => {
-  return <div></div>;
+  return (
+    <div className="h-screen bg-black flex flex-col-reverse gap-10 md:flex-row items-center justify-center lg:gap-52">
+      <div className="  text-white">
+        <img src={AI} alt="" />
+      </div>
+      <figure>
+        <img src={robot} alt="robot" />
+      </figure>
+    </div>
+  );
 };
 
 export default Banner;
