439cc460ddd72478ca5548f8293a6c48977219b5
FazleRifat121 <fazlerifatofficial@gmail.com>
2025-03-20 01:36:39 +0600

    sidebar added

--- src/shared/Navbar/Navbar.jsx
+++ src/shared/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import { NavLink } from "react-router";
 import logo from "../../assets/logo.png";
+import Sidebar from "./Sidebar";
 const Navbar = () => {
   const links = (
     <>
@@ -32,30 +33,8 @@ const Navbar = () => {
   return (
     <div className="navbar font-inter ">
       <div className="navbar-start">
-        <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              {" "}
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />{" "}
-            </svg>
-          </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-5 w-52 p-5 shadow gap-4"
-          >
-            {links}
-          </ul>
+        <div className="md:hidden mr-10">
+          <Sidebar></Sidebar>
         </div>
         <a href="/">
           <img src={logo} className="w-7xl md:w-full " alt="logo" />
---
+++ src/shared/Navbar/Sidebar.jsx
@@ -0,0 +1,85 @@
+import { MdClose } from "react-icons/md";
+import { NavLink } from "react-router";
+
+const Sidebar = () => {
+  const links = (
+    <>
+      <NavLink to={"/"}>
+        <li>Home</li>
+      </NavLink>
+      <NavLink to={"/about"}>
+        <li>About us</li>
+      </NavLink>
+      <NavLink to={"/about"}>
+        <li>Team</li>
+      </NavLink>
+      <NavLink to={"/about"}>
+        <li>Sales details</li>
+      </NavLink>
+      <NavLink to={"/about"}>
+        <li>Story</li>
+      </NavLink>
+      <NavLink to={"/about"}>
+        <li>How-to videos</li>
+      </NavLink>
+      <NavLink
+        to={"/about"}
+        className="btn bg-[#C8DA27] text-black rounded text-xl  font-inter py-6 px-4 lg:ml-5"
+      >
+        <li>Contact</li>
+      </NavLink>
+    </>
+  );
+  return (
+    <div className="z-10">
+      <div className="drawer">
+        <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content">
+          {/* Page content here */}
+          <label htmlFor="my-drawer">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost drawer-button"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                {" "}
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />{" "}
+              </svg>
+            </div>
+          </label>
+        </div>
+        <div className="drawer-side">
+          <label
+            htmlFor="my-drawer"
+            aria-label="close sidebar"
+            className="drawer-overlay"
+          ></label>
+          <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4 space-y-5">
+            {/* Sidebar content here */}
+            {links}
+
+            <label htmlFor="my-drawer">
+              <div className="btn mt-10 hidden show-on-mobile drawer-button text-lg w-20 font-bold text-white">
+                <MdClose />
+              </div>
+            </label>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
