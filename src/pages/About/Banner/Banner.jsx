983f09e43c31ee17107e1e0e2a175de8580a5184
FazleRifat121 <fazlerifatofficial@gmail.com>
2025-03-20 16:43:01 +0600

    trying to make it responsive

--- src/pages/About/Banner/Banner.jsx
+++ src/pages/About/Banner/Banner.jsx
@@ -1,23 +1,28 @@
 import Navbar from "../../../shared/Navbar/Navbar";
 import "./animatedBg.css";
+
 const Banner = () => {
   return (
-    <div className="relative min-h-[400px] max-w-screen overflow-hidden  ">
+    <div className="relative min-h-[400px] md:min-h-[500px] lg:min-h-[600px] max-w-screen overflow-hidden">
       {/* Animated Gradient Background */}
-      <div className="absolute inset-0 bg-gradient-to-r from-[#131313] via-[#313411] to-[#393E0E] animate-gradient"></div>
+      <div className="absolute inset-0 bg-gradient-to-r from-[#131313] via-[#313411] to-[#393E0E] animate-gradient" />
 
-      {/* Content - Modified div */}
-      <div className="absolute inset-x-0 bottom-0 z-10 w-[300px] md:w-[700px] lg:w-[1377px] p-5 mx-auto h-[calc(100%-2.5rem)] border border-b-0 rounded-t-xl">
-        <div>
+      {/* Content Container */}
+      <div className="absolute inset-x-0  top-5 bottom-0 z-10 w-full max-w-screen-lg xl:max-w-screen-xl 2xl:max-w-[1377px] mx-auto h-full border border-b-0 rounded-t-xl px-4 md:px-8 lg:px-10 py-6">
+        <div className="h-full flex flex-col">
           <Navbar />
-          <div className="text-center text-white pt-20">
-            <h1 className="text-3xl text-gray-400 mb-8">
-              What is <br />
-              <span className="bg-clip-text text-transparent bg-gradient-to-r from-[#C8DA27] to-[#8F9C1C] text-5xl lg:text-7xl animated-gradient">
-                Evoca?
-              </span>
-            </h1>
-            <div className="w-24 h-1 bg-white mx-auto"></div>
+
+          {/* Main Content */}
+          <div className="flex-1 flex items-center justify-center">
+            <div className="text-center text-white w-full">
+              <h1 className="text-3xl md:text-4xl lg:text-5xl text-gray-400 mb-6 md:mb-8">
+                What is <br />
+                <span className="bg-clip-text text-transparent bg-gradient-to-r from-[#C8DA27] to-[#8F9C1C] text-5xl md:text-6xl lg:text-7xl xl:text-8xl block mt-4">
+                  Evoca?
+                </span>
+              </h1>
+              <div className="w-24 h-1 bg-white mx-auto" />
+            </div>
           </div>
         </div>
       </div>
