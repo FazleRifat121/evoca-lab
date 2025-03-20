8a759f1991c67dae37388d75ffaa14d37e3e741c
FazleRifat121 <fazlerifatofficial@gmail.com>
2025-03-21 00:09:40 +0600

    story banner bg-img added

--- src/pages/Story/Banner/Banner.jsx
+++ src/pages/Story/Banner/Banner.jsx
@@ -1,16 +1,15 @@
 import Navbar from "../../../shared/Navbar/Navbar";
-
+import "./bannerStyle.css";
 const Banner = () => {
   return (
-    <div className="relative min-h-[400px] md:min-h-[500px] lg:min-h-[600px] container mx-auto overflow-hidden rounded-b-[3rem]  border-0">
+    <div className="relative min-h-[400px] md:min-h-[500px] lg:min-h-[600px] container mx-auto overflow-hidden rounded-b-[3rem]  border-0 bg-img">
       {/* Animated Gradient Background */}
-      <div className="absolute inset-0 bg-gradient-to-r from-[#131313] via-[#313411] to-[#393E0E] animate-gradient" />
+      <div className="absolute inset-0  bg-gradient-to-r from-[#131313] via-[#313411]/80 to-[#393E0E]/80  opacity-30" />
 
       {/* Content Container */}
       <div className="absolute inset-x-0  top-5 bottom-0 z-10 w-full max-w-screen-lg xl:max-w-screen-xl 2xl:max-w-[1377px] mx-auto h-full   px-4 md:px-8 lg:px-10 py-6">
         <div className="h-full flex flex-col">
           <Navbar />
-
           {/* Main Content */}
           <div className="flex-1 flex items-center justify-center">
             <div className=" text-white w-full">
