54814865f725718de94fbb1ea2816fd78cc7f5de
FazleRifat121 <fazlerifatofficial@gmail.com>
2025-03-21 22:28:13 +0600

    form added

--- src/pages/Contact/Banner/Banner.jsx
+++ src/pages/Contact/Banner/Banner.jsx
@@ -4,13 +4,13 @@ import { SiMinutemailer } from "react-icons/si";
 import { BiSolidPhoneCall } from "react-icons/bi";
 const Banner = () => {
   return (
-    <div className="md:ml-10 lg:ml-32 mt-20  h-[300px] lg:h-[600px]  flex flex-col items-start justify-center hero">
+    <div className="md:ml-10 lg:ml-32 mt-20  h-[300px] lg:h-[600px]  flex flex-col items-start justify-center hero h-[100vh">
       <div className=" flex items-center justify-between">
         <div className=" text-white w-full">
           <h1 className="text-6xl text-white">Contact</h1>
         </div>
       </div>
-      <div className="mt-20 md:mt-32 flex flex-col lg:flex-row justify-center items-baseline   ">
+      <div className="mt-20 md:mt-32 flex flex-col lg:flex-row justify-center items-baseline lg:gap-56  ">
         <div className="max-w-2xl mx-auto space-y-8">
           <div className="space-y-4 text-white">
             <h1 className="text-3xl font-light ">Write us a message</h1>
--- src/pages/Contact/Banner/Form.jsx
+++ src/pages/Contact/Banner/Form.jsx
@@ -1,5 +1,44 @@
 const Form = () => {
-  return <div></div>;
+  return (
+    <div className="max-w-5xl mx-5 lg:mx-auto mt-10 lg:mt-0  lg:mb-0">
+      <form action="" method="post" className="space-y-5">
+        <div className="lg:space-x-5 flex items-center flex-col lg:flex-row space-y-5 lg:space-y-0">
+          <input
+            type="text"
+            placeholder="Name"
+            className="input-group border border-gray-600 bg-[#1C1D1C] p-2 rounded"
+          />
+          <input
+            type="email"
+            placeholder="Email"
+            className="input-group border border-gray-600 bg-[#1C1D1C] p-2 rounded md:w-56 w-full"
+          />
+        </div>
+        <div>
+          <input
+            type="number"
+            placeholder="Phone"
+            className="input-group border border-gray-600 bg-[#1C1D1C] p-2 rounded w-full"
+          />
+        </div>
+        <div>
+          <textarea
+            className="w-full px-3 py-2 md:px-4 md:py-2.5 border border-gray-600 bg-[#1C1D1C] rounded focus:ring-2 focus:ring-blue-500 h-32 md:h-40 text-sm md:text-base"
+            required
+            placeholder="Message"
+          ></textarea>
+        </div>
+        <div>
+          <button
+            type="submit"
+            className="w-full bg-[#C8DA27] text-white py-2.5 md:py-3 px-6 rounded-lg hover:bg-[#c8da278f] transition-colors font-medium text-sm md:text-base"
+          >
+            Save Message
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Form;
