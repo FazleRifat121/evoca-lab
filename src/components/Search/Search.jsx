83a4363904eed4d1ff16338a408b88b6b0442ad3
FazleRifat121 <fazlerifatofficial@gmail.com>
2025-03-24 03:50:49 +0600

    searchbar fixed

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -2,10 +2,10 @@ import { IoSearchOutline } from "react-icons/io5";
 const Search = () => {
   return (
     <div>
-      <div className="relative">
+      <div className="relative z-0">
         <input
           type="text"
-          className="bg-base-300 focus:outline-none w-full  md:w-80 py-3 rounded-lg z-0"
+          className="bg-base-300 focus:outline-none w-full  md:w-80 py-3 rounded-lg "
           placeholder="   Search all Columns...."
         />
         <IoSearchOutline className="absolute top-3 sm:left-64 xs:left-52 md:left-72 text-2xl" />
